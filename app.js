8f18ababfb2fb5282a3fe5c8aa8a57be5d9bd34e
Okinz <steven14812@gmail.com>
2021-02-06 12:20:08 -0500

    Implemented post schema with connection to mongoose DB

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
-const { reduceRight, forEach } = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 const app = express();
 
@@ -17,40 +15,44 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Array of posts objects
-let posts = [
-  { title: 'Day 1', content: '1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-  { title: 'Another Post', content: '2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-  { title: 'Short post', content: 'Hi! my name is Steven! Nice to meet you!'}
-];
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-app.get("/", function(req,res){
-  res.render("home", {
-    home: homeStartingContent,
-    posts: posts,
-  });
+const postSchema = {
+  title: String,
+  content: String
+}
+
+const Post = mongoose.model("post", postSchema);
+
+const post1 = new Post ({
+  title: 'Day 1', 
+  content: '1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 });
 
-/** get the post request and obtain the requested title.
- * compare it with the stored title and if it is correct
- * redirect it to a post.ejs containing the contents and titles
- * passed through here to there.
- */
-app.get("/post/:test", function(req, res){
-    const requestedTitle = _.lowerCase(req.params.test);
-    
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
+const post2 = new Post ({
+  title: 'Day 2', 
+  content: '2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+});
 
-    if(requestedTitle === storedTitle){
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-      })
+const post3 = new Post ({
+  title: 'Day 3', 
+  content: '3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+});
+
+// Array of posts objects
+let arrPosts = [post1,post2,post3];
+
+app.get("/", function(req,res){
+  Post.find({}, function(err, foundPosts){
+    if(err){
+      console.log(err);
     } else {
-      console.log("not found");
+      res.render("home", {
+        posts: foundPosts,
+      });
     }
-  })
+  });
 });
 
 app.get("/about", function(req,res){
@@ -70,13 +72,40 @@ app.get("/compose", function(req,res){
 });
 
 app.post("/", function(req,res){
-  const post = {
-    title: req.body.composedTitle,
-    content:req.body.postText,
-  }
-  posts.push(post);
-  console.log(posts);
+  let postTitle = req.body.composedTitle;
+  let postContent = req.body.postText;
+
+  const post = new Post ({
+    title: postTitle,
+    content: postContent,
+  });
+  
+  post.save();
   res.redirect("/");
+});
+
+
+/** 
+ * Get the post request and obtain the requested title.
+ * compare it with the stored title and if it is correct
+ * redirect it to a post.ejs containing the contents and titles
+ * passed through here to there.
+ */
+app.get("/post/:postID", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postID);
+  
+  arrPosts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if(requestedTitle === storedTitle){
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      })
+    } else {
+      console.log("not found");
+    }
+  });
 });
 
 app.listen(3000, function() {
